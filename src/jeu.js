43ffb2d33832ea34080da47b1fbaa47ec12f43bf
alexis.peyron@gmail.com <alexispeyron@alexis-2.home>
2019-04-10 16:35:04 +0200

    création cases non accessibles

--- src/jeu.js
+++ src/jeu.js
@@ -1,53 +1,53 @@
 
-export{jeu};
-import{carte} from './grille';
-import{nb_aleat, getIndexPerso, getIndex} from './principal'
-import{tenueDeGrille} from './tenueDeGrille' 
 
 
+import{TenueDeGrille} from './tenueDeGrille' 
 
-let ligne;
-let colonne;
-const longueur = 10;
-    
-class jeu{
+
+class Jeu {
    constructor(ligne, colonne){
-     this.ligne = ligne
-     this.colonne = colonne
-     this.app = $("#app");
-      this.tab_position = [];
+     this.ligne = ligne;
+     this.colonne = colonne;
+     this.app = document.getElementById('app');
+      this.longueurGrille = this.ligne * this.colonne;
    }
    creationGrille(){
+     console.log('hello')
       let table = document.createElement('table')
       let body = document.createElement('tbody')
+      table.appendChild(body)
 
-      for(let i=0 i < this.ligne; i++){
+      for(let i = 0; i < this.ligne; i++){
 
         let tr = document.createElement('tr')
         body.appendChild(tr)
-        for(let j = 0 j < this.colonne; j++){
+        for(let j = 0; j < this.colonne; j++){
         let td = document.createElement('td')
         td.setAttribute("data-x", j)
         td.setAttribute("data-y", i)
         td.id = "td-" + i+j
-        tr.appendChild('td')
+        tr.appendChild(td)
 
         }
       }
+      this.app.appendChild(table)
+      this.creerCaseNonAccess()
+    
    }
-CreerCaseNonAccess(){
-  let tenueDeGrille = new tenueDeGrille(null, null, null, this.longueurGrille)
+creerCaseNonAccess(){
+ /* let tenueDeGrille = new TenueDeGrille(null, null, null, this.longueurGrille)
   let kase = null
   for(let i = 0; i < 50; i++){
     kase = tenueDeGrille.parcourirTable()
     kase.style.backgroundColor = 'black'
     kase.setAttribute('donnees-caseaccess', 0)
-  }
+  }*/
+  console.log('hello')
 }
 
-  }
+}
 
-
+/*
    this.app.appendChild(table)
     this.createNoAccess()
 
@@ -123,14 +123,5 @@ CreerCaseNonAccess(){
         });
         });
       };
-
-
-
-  
-
-
-
-
-
-
- 
+*/
+export{Jeu}
