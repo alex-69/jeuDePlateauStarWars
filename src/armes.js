37000729f57d81b389ce2ddd4b9f25477f2a48c5
alexis.peyron@gmail.com <alexispeyron@alexis-2.home>
2019-04-11 09:19:11 +0200

    caractéristiques armes

--- src/armes.js
+++ src/armes.js
@@ -4,10 +4,41 @@
 class Arme{
   constructor(){
     this.armeTab = [
-      {"nom": "sabreRouge",
-       "dégats": 5 
-    },
-    ]
+      {
+        "nom": "sabreRouge",
+       "dégats": 20
+      },
+      {
+        "nom": "SabreBleu",
+        "dégats": 20
+      },
+      {
+        "nom": "Blaster",
+        "dégats": 10
+      },
+      {
+        "nom": "Eclair",
+        "dégats": 30
+      },
+      {
+        "nom": "Arbalète laser",
+        "dégats": 15
+      },
+      {
+        "non": "Bâton balster",
+        "dégats":15
+      }
+    ];
+  }
+
+  SelectionneArme()
+  {
+    return this.armeTab
+  }
+
+  SelectionneNomAleatoire(aleatInt)
+  {
+    this.armeTab[aleatInt].name
   }
 }
 export {Arme};
