9e5e70c18535db98172cacb460e460203fd4295b
alexis.peyron@gmail.com <alexispeyron@alexis-2.home>
2019-05-22 09:19:23 +0200

    class combat

--- src/combat.js
+++ src/combat.js
@@ -2,41 +2,57 @@ class Combat{
 
     combattre(idBouton, joueurTab, armeTab){
 
+        
         let joueur1 = null
         let armeDegat = null
-
         let joueur2 = null
-         
+        
         //selectionne qui peut combattre
         for (let i = 0; i < joueurTab.length; i ++){
 
             if(joueurTab[i].combattre == true){
                 
                 joueur1 = joueurTab[i]
+               
+                if((joueur1).nom == 'Luke'){
+                    $('#attaquer-luke').css('visibility', 'hidden')
+                    $('#défendre-luke').css('visibility', 'hidden')
+                    $('#attaquer-vador').css('visibility', 'visible')
+                    $('#défendre-vador').css('visibility', 'visible')
+                }else if ((joueur1).nom == 'Vador'){
+                    $('#attaquer-luke').css('visibility', 'visible')
+                    $('#défendre-luke').css('visibility', 'visible')
+                    $('#attaquer-vador').css('visibility', 'hidden')
+                    $('#défendre-vador').css('visibility', 'hidden')
+                }
+            }else if(joueurTab[i].combattre == false) {
                 
-
-            }else if (joueurTab[i].combattre == false){
-
                 joueur2 = joueurTab[i]
-                console.log(joueur2.action)
+
+                
             }
         }
-
+        
+        //récupère les la puissance de dégats de l'arme
         for (let i = 0; i < armeTab.length; i++){
 
             if(joueur1.arme == armeTab[i].nom){
                 armeDegat = armeTab[i].degats
-                console.log(armeDegat)
+               
                
             }
         }
       
-        //le joueur attaque
-        if(idBouton.id == "attaquer"){
-           
+        //Si luke attaque
+        if(idBouton.id == "attaquer-luke" ^ idBouton.id == "attaquer-vador"){
+            
                 if(joueur2.action == "defendre" ){
                     joueur2.force = joueur2.force - (armeDegat/2)
-                  
+                   if(joueur2.nom == 'Luke'){
+                        $('#vie-luke').text(joueur2.force + ' points de vie')
+                   }else if(joueur2.nom == 'Vador'){
+                        $('#vie-vador').text(joueur2.force + ' points de vie')
+                   }
                     if(joueur2.force > 0){
                         joueur1.combattre = false
                         joueur2.combattre = true
@@ -44,28 +60,44 @@ class Combat{
                         
                     }else{
                         alert(joueur2.nom + ' est mort')
+                        $('button').css('visibility', 'hidden')
+                        if(joueur2.nom == 'Luke'){
+                            $('#vie-luke').text(joueur2.nom + ' est mort')
+                        }else if(joueur2.nom == 'Vador'){
+                            $('#vie-vador').text(joueur2.nom + ' est mort')
+                        }
+                        
                     }
 
                 }else if (joueur2.action == "attaquer"){
 
                     joueur2.force = joueur2.force - armeDegat
-                    console.log(joueur2.force)
-                    console.log(joueur2.nom)
-
+                    if(joueur2.nom == 'Luke'){
+                        $('#vie-luke').text(joueur2.force + ' points de vie')
+                    }else if(joueur2.nom == 'Vador'){
+                        $('#vie-vador').text(joueur2.force + ' points de vie')
+                    }
+                   
                     if(joueur2.force > 0){
 
                         joueur1.combattre = false
                         joueur2.combattre = true
-                        joueur1.action = "attaquer" 
-                        
+                        joueur1.action = "attaquer"    
 
                     }else{
                         alert(joueur2.nom + ' est mort')
+                        $('button').css('visibility', 'hidden')
+                        if(joueur2.nom == 'Luke'){
+                            $('#vie-luke').text(joueur2.nom + ' est mort')
+                        }else if(joueur2.nom == 'Vador'){
+                            $('#vie-vador').text(joueur2.nom + ' est mort')
+                        }
+                        $('#vie-luke').text(joueur2.nom + ' est mort')
                     }
                 }
                 
         //Le joueur se défend
-        }else if (idBouton.id == "défendre"){
+        }else if (idBouton.id == "défendre-luke" ^ idBouton.id == "défendre-vador"){
 
                 joueur1.action = "defendre" 
                 joueur1.combattre = false
@@ -74,6 +106,7 @@ class Combat{
         }
     
     }
+
 }
 
 export{Combat}
